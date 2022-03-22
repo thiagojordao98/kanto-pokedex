a7ccf1d932b98edc296ada86eafaed696b94a052
Thiago Jordão <thiagojordao98@gmail.com>
2022-03-21 23:34:52 -0300

    add scriptjs

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+/*
+- remover a classe aberto só do cartão que está aberto
+- ao clicar em um pokémon da listagem pegamos o id desse pokemon pra saber qual cartão mostrar
+- remover a classe ativo que marca o pokémon selecionado
+- adicionar a classe ativo no item da lista selecionado
+*/
+
+const listaSelecaoPokemons = document.querySelectorAll('.pokemon')
+console.log(listaSelecaoPokemons)
